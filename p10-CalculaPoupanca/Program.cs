9c24a357136fb949f3a125eb8105dd62105c95f3
Leandro Feitoza <p22.leandro@webmotors.com.br>
2021-10-18 15:38:16 -0300

    While

---
+++ p10-CalculaPoupanca/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace p10_CalculaPoupanca
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Executando projeto 10");
+
+			double valorInvestido = 1000;
+			double taxaDeJuros = 0.0036;
+			int contadorMes = 1;
+
+			while (contadorMes <= 12)
+			{
+				valorInvestido += valorInvestido * taxaDeJuros;
+				Console.WriteLine($"Após {contadorMes} meses, você terá R${valorInvestido}");
+				contadorMes++;
+			}
+		}
+	}
+}
