25acfdb1d815ce81bdc962d6dc68d58e57cd2384
Leandro Feitoza <p22.leandro@webmotors.com.br>
2021-10-18 15:44:18 -0300

    laco for

--- p11-CalculaPoupanca2/Program.cs
+++ p11-CalculaPoupanca2/Program.cs
@@ -1,22 +1,20 @@
 ﻿using System;
 
-namespace p10_CalculaPoupanca
+namespace p11_CalculaPoupanca2
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Executando projeto 10 - laço while");
+			Console.WriteLine("Executando o projeto 11 - laço for");
 
 			double valorInvestido = 1000;
 			double taxaDeJuros = 0.0036;
-			int contadorMes = 1;
 
-			while (contadorMes <= 12)
+			for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
 			{
 				valorInvestido += valorInvestido * taxaDeJuros;
 				Console.WriteLine($"Após {contadorMes} meses, você terá R${valorInvestido}");
-				contadorMes++;
 			}
 		}
 	}
