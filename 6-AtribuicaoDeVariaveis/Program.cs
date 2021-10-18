3bf1ad2680332743010a07ba5fa4e8ea27a87dc3
Leandro Feitoza <p22.leandro@webmotors.com.br>
2021-10-18 12:12:52 -0300

    Condicionais

---
+++ 6-AtribuicaoDeVariaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _6_AtribuicaoDeVariaveis
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Executando o projeto 6 - Atribuição de variáveis");
+
+			int idade = 40;
+			int idadeDoJoao = 30;
+
+			idade = 80;
+
+			Console.WriteLine(idade);
+			Console.WriteLine(idadeDoJoao);
+		}
+	}
+}
---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _7_Condicionais
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Executando projeto 7 - Condicionais");
+
+			int idade = 20;
+			int quantidadeDePessoas = 2;
+
+			if (idade >= 18)
+			{
+				Console.WriteLine("Idade maior que 18 anos");
+				Console.WriteLine("Seja bem vindo!");
+			}
+			else
+			{
+				if (quantidadeDePessoas >= 2)
+				{
+					Console.WriteLine("Voc~e não posssui 18 anos, mas pode entrar, pois está acompanhado");
+				}
+				else
+				{
+					Console.WriteLine("Inelizemente você não pode entrar");
+				}
+			}
+		}
+	}
+}
