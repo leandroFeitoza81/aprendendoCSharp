f41e5c98d5a9098329f2a5d4c6b66ef081827c86
Leandro Feitoza <p22.leandro@webmotors.com.br>
2021-10-19 09:26:48 -0300

    laços de repetição

---
+++ LacoDeRepetiçãoFor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LacoDeRepetiçãoFor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Executando projeto - Laço e repetição For");
+
+			for (int contador = 0; contador < 10; contador ++)
+			{
+				Console.WriteLine("O valor do contador é " + contador);
+				
+			}
+		}
+	}
+}
--- p13-ForEncadeado/Program.cs
+++ p13-ForEncadeado/Program.cs
@@ -7,6 +7,30 @@ namespace p13_ForEncadeado
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Executandio o projeto 13");
+
+			// Usando o break para desenhar os asteriscos
+			for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+			{
+				for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+				{
+					Console.Write("*");
+					if (contadorColuna >= contadorLinha)
+						break;
+				}
+				Console.WriteLine();
+			}
+
+			Console.WriteLine("==============================");
+			Console.WriteLine("Sem o break");
+
+			for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+			{
+				for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine();
+			}
 		}
 	}
 }
