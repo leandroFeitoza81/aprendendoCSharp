0a1b8bb2852e98be8e1d81cf18b5d43c746a6d1b
Leandro Feitoza <p22.leandro@webmotors.com.br>
2021-10-21 18:02:10 -0300

    MÃetodos

---
+++ introducao-POO/ByteBank/04-ByteBank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensagemPositivo = "Saque efetuado! Saldo atual é: ";
+            string mensagemNegativo = "Saque NÃO efetuado! Saldo atual é: ";
+
+            ContaCorrente ContaDoJoao = new ContaCorrente();
+
+            Console.WriteLine(ContaDoJoao.saldo);
+            bool resultadoDoSaque = ContaDoJoao.Sacar(500);
+            if (resultadoDoSaque)
+            {
+                Console.WriteLine(mensagemPositivo + ContaDoJoao.saldo); 
+            }
+            else
+            {
+                Console.WriteLine(mensagemNegativo + ContaDoJoao.saldo);
+            }
+        }
+    }
+}
