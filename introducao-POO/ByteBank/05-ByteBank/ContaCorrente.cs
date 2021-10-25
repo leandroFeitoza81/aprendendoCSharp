e57ab9d088df8f5f2af38ce1520bd80965743992
Leandro <leandro.feitoza8018@gmail.com>
2021-10-25 11:51:45 -0300

    composicao de e relacionamento entre classes

---
+++ introducao-POO/ByteBank/05-ByteBank/ContaCorrente.cs
@@ -0,0 +1,41 @@
+﻿using _05_ByteBank;
+public class ContaCorrente
+{
+//	public string titular;
+	public int agencia;
+	public int numero;
+	public double saldo = 100;
+    public Cliente titular;
+
+	public bool Sacar(double valor)
+    {
+		if (this.saldo < valor)
+        {
+			return false;
+        }
+        else
+        {
+            saldo -= valor;
+            return true;    
+        }
+    }
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+    }
+
+    public bool Transferir(double valor, ContaCorrente contaDestino)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
---
+++ introducao-POO/ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _05_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaCamila = new ContaCorrente();
+            contaDaCamila.titular = new Cliente();
+
+            contaDaCamila.titular.nome = "Camila";
+        }
+    }
+}
