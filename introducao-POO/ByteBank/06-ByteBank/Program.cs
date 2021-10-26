3f55eabfbbba770797788697a6485739184d9ab2
Leandro <leandro.feitoza8018@gmail.com>
2021-10-26 10:03:16 -0300

    getters and setters

---
+++ introducao-POO/ByteBank/06-ByteBank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.DefinirSaldo(-20);
+
+            Console.WriteLine(conta.ObterSaldo());
+        }
+    }
+}
